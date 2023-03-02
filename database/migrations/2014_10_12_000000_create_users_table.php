5361ab38b1e342a48a93e6e848df3154dea40dc2
postcoder <b.yakupov@internet.ru>
2023-03-03 01:55:24 +0300

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->strng('isManager')->default('no');
+            $table->string('isManager')->default('no');
             $table->rememberToken();
             $table->timestamps();
         });
